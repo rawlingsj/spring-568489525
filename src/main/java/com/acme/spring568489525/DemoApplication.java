4d58e26dca55528d69a39c6ee35ac90bbb0c792b
rawlingsj <rawlingsj80@gmail.com>
2018-02-02 12:44:20 +0000

    Initial import

---
+++ src/main/java/com/acme/spring568489525/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.acme.spring568489525;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
